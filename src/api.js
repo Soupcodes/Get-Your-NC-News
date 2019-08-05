b9d04320cfece531e74041dcec091c7a7797ccfa
Soupcodes <alantong789@gmail.com>
2019-08-05 14:24:46 +0100

    GET articles from server

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const request = axios.create({
+  baseURL: "https://alansoup-nc-news.herokuapp.com/api"
+});
+
+export const getArticles = async query => {
+  const { data } = await request.get("/articles");
+  return data.articles;
+};
+
+export const getUsers = async query => {
+  const { data } = await request.get("/articles");
+  return data.articles;
+};
