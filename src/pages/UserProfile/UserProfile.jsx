ea6fe60e16a9a4fba755df53554dc7fa86aa83cb
Soupcodes <alantong789@gmail.com>
2019-08-07 20:09:52 +0100

    Styling user profile page

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import * as api from "../../api";
 import LoadingSpinner from "../../components/LoadingSpinner";
 import DefaultErrorPage from "../../components/DefaultErrorPage";
+import styles from "./styles/UserProfile.module.css";
+import ArticleList from "../Homepage/ArticleList";
 
 class UserProfile extends Component {
   state = {
+    articles: null,
+    comments: null,
     user: null,
+    author: null,
     isLoading: true,
     errStatus: null,
     errMsg: null
@@ -19,11 +24,12 @@ class UserProfile extends Component {
 
     return (
       <ul>
-        <li>
-          <h1>LEFT ALIGN</h1>
-          <p>{user.name}</p>
+        <li className={styles.centerItems}>
+          <h3>
+            <u>User: {user.name}</u>
+          </h3>
           <img src={user.avatar_url} alt="profile-pic" />
-          <p>{user.username}</p>
+          <p>Username: {user.username}</p>
         </li>
       </ul>
     );
@@ -33,11 +39,40 @@ class UserProfile extends Component {
     this.fetchUser();
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const { username } = this.state.user;
+    if (this.state.user !== prevState.user) {
+      api.getArticles({ author: username }).then(articles => {
+        this.setState({ articles });
+      });
+    }
+  }
+
   fetchUser = () => {
     const { username } = this.props;
     api
       .getUser(username)
-      .then(user => this.setState({ user, isLoading: false }))
+      .then(user =>
+        this.setState({ user, isLoading: false, author: user.username })
+      )
+      .catch(({ response }) =>
+        this.setState({
+          errStatus: response.status,
+          errMsg: response.data.msg,
+          isLoading: false
+        })
+      );
+  };
+
+  fetchArticles = () => {
+    api
+      .getArticles()
+      .then(articles =>
+        this.setState({
+          articles,
+          isLoading: false
+        })
+      )
       .catch(({ response }) =>
         this.setState({
           errStatus: response.status,
