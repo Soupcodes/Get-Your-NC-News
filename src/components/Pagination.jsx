35e1e631c5e2053e97f1fe043774a206dae42e41
Soupcodes <alantong789@gmail.com>
2019-08-08 22:22:28 +0100

    Implemented Pagination to Homepage

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ChangePage = ({ page, browsePage }) => {
+  const handleClick = inc_page => {
+    browsePage(inc_page);
+  };
+
+  return (
+    <>
+      <button onClick={() => handleClick(-1)} disabled={page === 1}>
+        Back
+      </button>
+      <button onClick={() => handleClick(1)}>Next</button>
+    </>
+  );
+};
+export default ChangePage;
