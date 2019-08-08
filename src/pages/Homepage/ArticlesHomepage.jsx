9abc4e79a10c177e09903f9ac34e21878a5ecbe3
Soupcodes <alantong789@gmail.com>
2019-08-08 14:49:21 +0100

    Pagination to next page implemented
    Work for clicking to next page, and disabled back on page 1

--- src/pages/Homepage/ArticlesHomepage.jsx
+++ src/pages/Homepage/ArticlesHomepage.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import ArticleList from "./ArticleList";
+import ChangePage from "../../components/Pagination";
+import * as api from "../../api";
 
-const ArticlesHomepage = () => {
-  return (
-    <>
-      <h1>Latest</h1>
-      <ArticleList />
-    </>
-  );
-};
+class ArticlesHomepage extends Component {
+  state = {
+    page: 1
+  };
+
+  render() {
+    const { page } = this.state;
+
+    return (
+      <>
+        <h1>Latest</h1>
+        <ArticleList p={page} />
+        <p>{page}</p>
+        <ChangePage page={page} browsePage={this.browsePage} />
+      </>
+    );
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { page } = this.state;
+
+    if (prevState.page !== page) {
+      api.getArticles({ p: page });
+    }
+  }
+
+  browsePage = inc_votes => {
+    this.setState(currentState => {
+      console.log(currentState.page + inc_votes, "<---");
+      return { page: currentState.page + inc_votes };
+    });
+  };
+}
 
 export default ArticlesHomepage;
