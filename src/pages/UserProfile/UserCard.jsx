37218e2e14e060db2a18244a73529e044b092938
Soupcodes <alantong789@gmail.com>
2019-08-08 22:44:25 +0100

    Added Articles to user profile page

---
+++ src/pages/UserProfile/UserCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./styles/UserCard.module.css";
+
+const UserCard = ({ user }) => {
+  return (
+    <ul>
+      <li className={styles.centerItems}>
+        <h3>
+          <u>User: {user.name}</u>
+        </h3>
+        <img src={user.avatar_url} alt="profile-pic" />
+        <p>Username: {user.username}</p>
+      </li>
+    </ul>
+  );
+};
+
+export default UserCard;
---
+++ src/pages/UserProfile/UserProfilePage.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import * as api from "../../api";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import DefaultErrorPage from "../../components/DefaultErrorPage";
+import UserCard from "./UserCard";
+import ArticleCard from "../Homepage/ArticleCard";
+
+class UserProfile extends Component {
+  state = {
+    articles: null,
+    comments: null,
+    user: null,
+    author: null,
+    isLoading: true,
+    errStatus: null,
+    errMsg: null
+  };
+
+  render() {
+    const { user, isLoading, errStatus, errMsg, articles } = this.state;
+    if (isLoading) return <LoadingSpinner />;
+    if (errStatus)
+      return <DefaultErrorPage errStatus={errStatus} errMsg={errMsg} />;
+
+    return (
+      <>
+        <UserCard user={user} />
+        {articles ? (
+          articles.map(article => {
+            return <ArticleCard article={article} key={article.article_id} />;
+          })
+        ) : (
+          <></>
+        )}
+      </>
+    );
+  }
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("updating");
+    const { username } = this.state.user;
+    if (this.state.user !== prevState.user) {
+      api.getArticles({ author: username }).then(articles => {
+        this.setState({ articles });
+      });
+    }
+  }
+
+  fetchUser = () => {
+    const { username } = this.props;
+    api
+      .getUser(username)
+      .then(user =>
+        this.setState({ user, isLoading: false, author: user.username })
+      )
+      .catch(({ response }) =>
+        this.setState({
+          errStatus: response.status,
+          errMsg: response.data.msg,
+          isLoading: false
+        })
+      );
+  };
+
+  fetchArticles = () => {
+    api
+      .getArticles()
+      .then(articles =>
+        this.setState({
+          articles,
+          isLoading: false
+        })
+      )
+      .catch(({ response }) =>
+        this.setState({
+          errStatus: response.status,
+          errMsg: response.data.msg,
+          isLoading: false
+        })
+      );
+  };
+}
+
+export default UserProfile;
